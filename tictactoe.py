835b1ae153670726c3e2d959e36db17cd1a43aa1
stenman <roger.stenman@gmail.com>
2020-04-14 14:14:56 +0200

    Added board order

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,7 @@
 import random
 
-# x/o vartannat, position rnd 1-9 tills siffrorna "är slut"
-# efter varje placering: kolla om någon vunnit
-
 board = [None] * 9
+boardOrder = [None] * 9
 x = set()
 o = set()
 winningBoards = [{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}]
@@ -12,27 +10,27 @@ def oneGame():
     shuffled = list(range(9))
     random.shuffle(shuffled)
     for i in range(0, 9, 2):
-        print(i)
-        placeMarker("x", shuffled[i])
+        placeMarker("x", shuffled[i], i)
         if(checkWinner()):
             break
         if(i < 8):
-            placeMarker("o", shuffled[i+1])
+            placeMarker("o", shuffled[i+1], i+1)
         if(checkWinner()):
             break
     
-def placeMarker(m, p):
+def placeMarker(m, p, i):
     board[p] = m
+    boardOrder[p] = i
 
 def checkWinner():
-    print(board)
+    ## print(board)
     for i in range(9):
         if(board[i] == "x"):
             x.add(i)
         if(board[i] == "o"):
             o.add(i)
-    print(x)
-    print(o)
+    ## print(x)
+    ## print(o)
     for i in winningBoards:
         if(x.issuperset(i)):
             print("Winner x!")
@@ -42,15 +40,17 @@ def checkWinner():
             return True
     return False
 
-def printBoard():
+def printBoard(title, board):
     finalBoard = [" " if i is None else i for i in board]
-    print("\nEnd Board")
+    print(title)
     print("-------")
-    print("|" + finalBoard[0] + "|" + finalBoard[1] + "|" + finalBoard[2] + "|")
-    print("|" + finalBoard[3] + "|" + finalBoard[4] + "|" + finalBoard[5] + "|")
-    print("|" + finalBoard[6] + "|" + finalBoard[7] + "|" + finalBoard[8] + "|")
-    print("-------")
+    print("|" + str(finalBoard[0]) + "|" + str(finalBoard[1]) + "|" + str(finalBoard[2]) + "|")
+    print("|" + str(finalBoard[3]) + "|" + str(finalBoard[4]) + "|" + str(finalBoard[5]) + "|")
+    print("|" + str(finalBoard[6]) + "|" + str(finalBoard[7]) + "|" + str(finalBoard[8]) + "|")
+    print("-------\n")
 
-oneGame()
-printBoard()
+for i in range(3):
+    oneGame()
+    printBoard("End Board", board)
+    printBoard("Order", boardOrder)
 
